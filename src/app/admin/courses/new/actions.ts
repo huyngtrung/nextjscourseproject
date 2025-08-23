9bf9835230d0d6c476072c570300a9d853dc201e
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2025-08-23 16:45:42 +0700

    bridge setting

--- src/app/admin/courses/new/actions.ts
+++ src/app/admin/courses/new/actions.ts
@@ -2,13 +2,14 @@
 
 import { createCourse, updateCourse, deleteCourse } from '@/features/courses/actions/courses';
 import { courseSchema } from '@/features/courses/schemas/courses';
+import { redirect } from 'next/navigation';
 import z from 'zod';
 
 type CourseInput = z.infer<typeof courseSchema>;
 
 export async function createCourseAction(data: CourseInput) {
     const course = await createCourse(data);
-    return { success: true, course: JSON.parse(JSON.stringify(course)) };
+    redirect(`/admin/courses/${course.id}/edit`);
 }
 
 export async function updateCourseAction(id: string, data: CourseInput) {
